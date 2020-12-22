aacc00acef4cd22ee6dfaa69d9f4a9541624b6b7
DaangamesDG <dwklarenbeek@gmail.com>
2020-12-22 17:20:59 +0100

    added fun commands

---
+++ src/commands/Fun/8ballCommand.ts
@@ -0,0 +1,47 @@
+import { Message } from 'discord.js';
+import BaseCommand from '../../utils/structures/BaseCommand';
+import DiscordClient from '../../client/client';
+
+const ball: Array<string> = [
+  '🎱 | As I see it, yes,', 
+  '🎱 | Better not tell you now,',  
+  '🎱 | Cannot predict now,' , 
+  '🎱 | Concentrate and ask again,' , 
+  '🎱 | Don’t count on it,' , 
+  '🎱 | It is certain,' , 
+  '🎱 | It is decidedly so,' , 
+  '🎱 | Most likely,' , 
+  '🎱 | My reply is no,' , 
+  '🎱 | My sources say no,' , 
+  '🎱 | Outlook not so good,' , 
+  '🎱 | Outlook good,' , 
+  '🎱 | Reply hazy, try again,' , 
+  '🎱 | Signs point to yes,' , 
+  '🎱 | Very doubtful,' , 
+  '🎱 | Without a doubt,' , 
+  '🎱 | You may rely on it,' , 
+  '🎱 | Yes,' , 
+  '🎱 | Yes – definitely,' ,
+];
+
+
+export default class eightballCommand extends BaseCommand {
+  constructor() {
+    super('eightball', {
+      category: 'Fun',
+      aliases: ['8ball'],
+      description: "8ball will answer all your questions",
+      usage: "<question including ?>",
+      channelType: "both",
+      ownerOnly: false,
+    });
+  }
+
+  async run(client: DiscordClient, message: Message, args: Array<string>) {
+    if (!args.map(a => a).join(' ').toString().endsWith('?'))
+      return message.channel.send("> ⁉ | I can not answer your question without one (add a ? to your question)");
+      
+    const choice = ball[Math.floor(Math.random() * ball.length)];
+    return message.channel.send(`> ${choice} **${message.author.username}**.`);
+  }
+}
---
+++ src/commands/Fun/emojifyCommand.ts
@@ -0,0 +1,20 @@
+import { Message } from 'discord.js';
+import BaseCommand from '../../utils/structures/BaseCommand';
+import DiscordClient from '../../client/client';
+
+export default class emojifyCommand extends BaseCommand {
+  constructor() {
+    super("emojify", {
+      category: 'Fun',
+      aliases: [],
+      description: "emojify your text with this command.",
+      usage: "<text>",
+      channelType: "both",
+      ownerOnly: false,
+    });
+  }
+
+  async run(client: DiscordClient, message: Message, args: Array<string>) {
+    return message.channel.send(`> 🔤 | ${client.utils.emojify(args.join(' '))}`);
+  }
+}
