c12d0dcb2213c92a14fe9983e25638be446f173b
DaangamesDG <dwklarenbeek@gmail.com>
2020-12-24 13:02:28 +0100

    added notification system

--- config.ts
+++ config.ts
@@ -4,3 +4,16 @@ export const systemLog: string = "781841366795288576";
 
 // roles
 export const muteRole: string = "739546067984580639";
+
+// youtube notifications
+export const channelIds: string[] = [];
+export const DraavoMsg: string = "<@&709908903135019010> \n**{channelName}** just posted an **EPIC** video! Make sure to check it out below!\n{link}";
+export const seniorTeamMsg: string = "<@&761271646152818739>\n**{channelName}** just posted a new video! Check it out:\n{link}";
+
+// yt-webhook-draavo
+export const ytId: string = "";
+export const ytToken: string = "";
+
+// yt-webhook-senior
+export const ytIdS: string = "";
+export const ytTokenS: string = "";
---
+++ src/api/youtube/notifications.ts
@@ -0,0 +1,59 @@
+import { channelIds, DraavoMsg, seniorTeamMsg, ytId, ytToken, ytIdS , ytTokenS } from "../../../config";
+
+// setup notifier
+import YouTubeNotifier from "youtube-notification";
+const domain: string = "";
+const notifier = new YouTubeNotifier({
+  hubCallback: `http://${domain}/yt`,
+  secret: "very_cool_secret"
+});
+
+
+notifier.on('notified', (data: Data) => {
+  data.channel.id === "UCkMrp3dJhWz2FcGTzywQGWg"
+  ? send(DraavoMsg.replace(/{channelName}/g, data.channel.name).replace(/{link}/g, data.video.link), data.channel.id)
+  : send(seniorTeamMsg.replace(/{channelName}/g, data.channel.name).replace(/{link}/g, data.video.link), data.channel.id);
+});
+ 
+notifier.subscribe(channelIds);
+
+// setup web server for callback
+import http from"https";
+import express from"express";
+import { WebhookClient } from "discord.js";
+
+const webhook = new WebhookClient(ytId, ytToken);
+const webhookS = new WebhookClient(ytIdS, ytTokenS);
+const app = express();
+
+const server = http.createServer(app);
+
+app.use("/yt", notifier.listener());
+server.listen(process.env.PORT, () =>
+  console.log("api online and listening to port " + process.env.PORT)
+);
+
+// interfaces
+interface Data {
+  video: {
+    id: string,
+  	title: string,
+    link: string,
+  },
+  channel: {
+    id: string,
+    name: string,
+    link: string,
+  },
+  published: Date,
+  updated: Date,
+}
+
+// functions
+function send(message: string, id: string) {
+  id === "UCkMrp3dJhWz2FcGTzywQGWg"
+  ? webhook.send(message)
+    .catch(e => console.log(e))
+  : webhookS.send(message)
+    .catch(e => console.log(e));
+}
